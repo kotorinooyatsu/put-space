c9c63d3d51a22ffd74acb75f5f738ffa12721a6f
Haruka Nishio <haru.nishi.314@gmail.com>
2023-10-01 16:39:57 +0900

    2023-10-01 update 入力された文字列に全角スペースを挿入する機能の追加

--- src/Component/ButtonForConverting.js
+++ src/Component/ButtonForConverting.js
@@ -2,8 +2,16 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 
 export default function ButtonForConverting() {
+  function handleClick() {
+    alert('Click')
+  }
+
   return (
-    <Button variant="contained" size="large">
+    <Button
+      variant="contained"
+      size="large"
+      onClick={handleClick}
+    >
       変換
     </Button>
   );
